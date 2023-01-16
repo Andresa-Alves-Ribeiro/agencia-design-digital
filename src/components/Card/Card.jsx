7e3f7c7948754468ecd8241dd063d8d39cb245a2
Andresa Alves <andresa_15ga@hotmail.com>
2023-01-16 17:18:35 -0300

    consertando elemento Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,29 +1,13 @@
 import './style.css';
 import Logo from '../../assets/logo.png';
 
-function Card() {
+function Card(props) {
     return (
-        <div>
-            <div>
-                <p>JUNHO 2012 - 2016</p>
-                <h3>Web Designer</h3>
-                <p>Pied Piper StartUp.</p>
-                <p>Criação de Landing pages, sites institucionais e E-commerces completamente personalizados e otimizados para buscadores</p>
-            </div>
-
-            <div>
-                <p>AGOSTO 2016 - 2019</p>
-                <h3>Product Designer</h3>
-                <p>E Corp.</p>
-                <p>Criação de modelos estratégicos de conversão identificando o cliente e mapeando toda a sua jornada de compra</p>
-            </div>
-
-            <div>
-                <p>FEVEREIRO 2019 - 2021</p>
-                <h3>Digital Consulting</h3>
-                <p>Arasaka Inc.</p>
-                <p>Consultoria em estratégias digitais para todas as etapas do ciclo do projeto, desde a definição inicial até a execução</p>
-            </div>
+        <div id="card" className="card-modo-claro">
+            <p className="primeiroParagrafo">{props.data}</p>
+            <h4>{props.titulo}</h4>
+            <p className="segundoParagrafo">{props.empresa}</p>
+            <p>{props.paragrafo}</p>
         </div>
     );
 }
